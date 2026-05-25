41e55b8fc5bd59dd46ffe5fe53e7f929e885e866
Linderlly  Santana <148879489+Linderlly@users.noreply.github.com>
2026-05-24 21:38:26 -0400

    Ajuste no about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,7 +26,6 @@ export default function About() {
               Sou estudante de Bacharelado em Engenharia de Software, atualmente em fase de conclusão da graduação, com formatura prevista para setembro de 2026. Apaixonado por tecnologia, desenvolvimento web e educação tecnológica, possuo experiência como instrutor de informática, atuando no planejamento e condução de aulas, suporte acadêmico e acompanhamento do desenvolvimento de alunos em diferentes áreas da tecnologia.
               Tenho conhecimentos em Desenvolvimento Web utilizando HTML, CSS, JavaScript, Node.js e Angular, além de noções com linguagens como Java, Python e C#. Também possuo familiaridade com bancos de dados como MySQL, PostgreSQL, Oracle e SQL Server.
               Ao longo da minha trajetória, desenvolvi habilidades importantes como comunicação, trabalho em equipe, liderança, criatividade e resolução de problemas, sempre buscando unir conhecimento técnico com uma abordagem prática, inovadora e didática.
-              Estou em busca de oportunidades para crescer profissionalmente na área de tecnologia, contribuindo com dedicação, aprendizado contínuo e desenvolvimento de soluções que gerem impacto positivo para pessoas e empresas.
             </p>
           </div>
         </div>
